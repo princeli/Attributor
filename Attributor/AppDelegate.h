89ae7e63d93994d32563e0591de4a52fcc1f9b03
princeli <princeli@live.cn>
2015-11-06 21:33:18 +0800

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by ly on 15/11/6.
+//  Copyright © 2015年 princeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
